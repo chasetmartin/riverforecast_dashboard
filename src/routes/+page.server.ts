58c8011d8ea75ae4b4fe925193107f26e01fce2b
chasetmartin <36861079+chasetmartin@users.noreply.github.com>
2023-12-07 15:28:47 -0800

    Flood zones and today value on graph

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -4,8 +4,11 @@ import type { PageServerLoad } from "./$types";
 export const load: PageServerLoad = async () => {
     const response = await fetch('https://preview-api.water.noaa.gov/v1/gauges/TLMO3/stageflow');
     const data = await response.json();
+    const response2 = await fetch('https://preview-api.water.noaa.gov/v1/gauges/TLMO3');
+    const data2 = await response2.json();
 
     return {
-        data
+        data,
+        data2
     };
 };
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,10 @@
 <script lang="ts">
     import { onMount } from 'svelte';
-    import * as d3 from 'd3';
+    import { Chart } from 'frappe-charts';
 
     export let data: any;
+
+    let myChart;
 
     // Process the API data and create chart datasets
     const observedData = data?.data?.observed?.data || [];
@@ -16,171 +18,133 @@
     const filteredObservedData = observedData
         .filter((entry: { validTime: string | number | Date; }) => new Date(entry.validTime) >= fiveDaysAgo && new Date(entry.validTime).getMinutes() === 0);
 
-    const observedLabels = filteredObservedData.map(entry => entry.validTime);
-    const forecastLabels = forecastData.map(entry => entry.validTime);
+    const observedLabels = filteredObservedData.map(entry => new Date(entry.validTime).toLocaleString('en-US', {
+        month: 'short',
+        day: 'numeric',
+        hour: 'numeric',
+        minute: 'numeric',
+        timeZone: 'America/Los_Angeles', // Adjust to your desired timezone
+    }));
+
+    const forecastLabels = forecastData.map(entry => new Date(entry.validTime).toLocaleString('en-US', {
+        month: 'short',
+        day: 'numeric',
+        hour: 'numeric',
+        minute: 'numeric',
+        timeZone: 'America/Los_Angeles', // Adjust to your desired timezone
+    }));
 
     const chartLabels = observedLabels.concat(forecastLabels);
 
     const observedValues = filteredObservedData.map((entry: { secondary: number; }) => entry.secondary * 1000);
-    const forecastValues = forecastData.map((entry: { secondary: number; }) => entry.secondary * 1000);
-    // Pad the forecast dataset with null values to match the length of the observed dataset
-    // const forecastValues = Array(filteredObservedData.length).fill(null).concat(forecastData.map((entry: { secondary: number; }) => entry.secondary * 1000));
-    
-    const addendedValues = observedValues.concat(forecastValues);
 
-    const svgWidth = 700;
-    const svgHeight = 400;
-    const margin = { top: 30, right: 5, bottom: 70, left: 150 };
+    // Pad the forecast dataset with null values to match the length of the observed dataset
+    const forecastValues = Array(filteredObservedData.length).fill(null).concat(forecastData.map((entry: { secondary: number; }) => entry.secondary * 1000));
+
+    // Bar for today
+    const todayValues = Array(forecastValues.length).fill(null);
+    todayValues[observedValues.length-1] = observedValues[observedValues.length - 1];
+
+    // Add 40 null values to the end of the observedValues array
+    const lengthToAdd = forecastValues.length - observedValues.length;
+    observedValues.length += lengthToAdd;
+    observedValues.fill(null, observedValues.length - lengthToAdd);
+    
+    //Flood stages
+    const actionFlood = data?.data2?.flood?.categories?.action?.flow;
+    const minorFlood = data?.data2?.flood?.categories?.minor?.flow;
+    const moderateFlood = data?.data2?.flood?.categories?.moderate?.flow;
+    const majorFlood = data?.data2?.flood?.categories?.major?.flow;
+
+    //River Info
+    const riverTitle = data?.data2?.name;
+
+    const chartdata = {
+        labels: chartLabels,
+        datasets: [
+            {
+                name: "Observed",
+                chartType: "bar",
+                values: observedValues,
+                color: '#7cd6fd' // Use a color for observed values
+            },
+            {
+                name: "Forecast",
+                chartType: "bar",
+                values: forecastValues,
+                color: '#ff2b5c' 
+            },
+            {
+                name: "Today",
+                chartType: "bar",
+                values: todayValues,
+                color: '#f91643'
+            }
+        ],
+        yRegions: [
+                {
+                    label: "Action Level",
+                    start: 0,
+                    end: actionFlood,
+                    options: { labelPos: 'left' }
+                },
+                {
+                    label: "Minor Flood",
+                    start: actionFlood,
+                    end: minorFlood,
+                    options: { labelPos: 'right' }
+                },
+                {
+                    label: "Moderate Flood",
+                    start: minorFlood,
+                    end: moderateFlood,
+                    options: { labelPos: 'right'}
+                },
+                {
+                    label: "Major Flood",
+                    start: moderateFlood,
+                    end: majorFlood,
+                    options: { labelPos: 'right'}
+                },
+                {
+                    label: "",
+                    start: majorFlood,
+                    end: majorFlood + 1500,
+                    options: { labelPos: 'right'}
+                }
+            ]
+    };
 
     onMount(() => {
-        const svg = d3.select('#myChart')
-      .append('svg')
-      .attr('width', svgWidth)
-      .attr('height', svgHeight);
+        myChart = new Chart("#myChart", {
+            data: chartdata,
+            type: 'bar',
+            height: 500,
+            colors: ['#743ee2', '#7cd6fd', '#f91643'],
+            axisOptions: {
+                xIsSeries: true,
+                xAxisMode: 'tick'
+            },
+            barOptions: {
+                spaceRatio: 0.01
+            },
+            lineOptions: {
+                spline: 1,
+                hideDots: 1,
+                heatline: 1,
+                regionFill: 1,
+            }
+        });
 
-    const xScale = d3.scaleLinear()
-      .domain([0, addendedValues.length - 1])
-      .range([margin.left, svgWidth - margin.right]);
-
-    const yScale = d3.scaleLinear()
-      .domain([0, d3.max(addendedValues)])
-      .range([svgHeight - margin.bottom, margin.top]);
-
-    const observedLine = d3.line()
-      .x((d, i) => xScale(i))
-      .y(d => yScale(d));
-
-      svg.append('path')
-    .data([observedValues])
-    .attr('d', observedLine)
-    .attr('stroke', '#7cd6fd')
-    .attr('stroke-width', 2)
-    .attr('fill', 'none')
-    .attr('class', 'line')
-    .each(function () {
-      // Animate the line drawing
-      const totalLength = this.getTotalLength();
-      d3.select(this)
-        .attr('stroke-dasharray', totalLength + ' ' + totalLength)
-        .attr('stroke-dashoffset', totalLength)
-        .transition()
-        .duration(1500)
-        .attr('stroke-dashoffset', 0);
-    });
-
-    const forecastLine = d3.line()
-      .x((d, i) => xScale(i + observedValues.length))
-      .y(d => yScale(d));
-
-      svg.append('path')
-    .data([forecastValues])
-    .attr('d', forecastLine)
-    .attr('stroke', '#fca130')
-    .attr('stroke-width', 2)
-    .attr('fill', 'none')
-    .attr('class', 'line')
-    .each(function () {
-      // Animate the line drawing
-      const totalLength = this.getTotalLength();
-      d3.select(this)
-        .attr('stroke-dasharray', totalLength + ' ' + totalLength)
-        .attr('stroke-dashoffset', totalLength)
-        .transition()
-        .duration(1500)
-        .attr('stroke-dashoffset', 0);
-    });
-
-    const dots = svg.selectAll('.dot')
-        .data(addendedValues)
-        .enter().append('circle')
-        .attr('class', 'dot')
-        .attr('r', 3.5)
-        .attr('cx', (d, i) => xScale(i))
-        .attr('cy', d => yScale(d))
-        .style('fill', (d, i) => i < observedValues.length ? '#7cd6fd' : '#fca130')
-        .style('opacity', 0) // Initial opacity set to 0
-      .on('mouseover', function (event, d) {
-        tooltip.transition()
-          .duration(200)
-          .style('opacity', .9);
-        tooltip.html(`Value: ${d}`)
-          .style('left', (event.x) + 'px')
-          .style('top', (event.y - 28) + 'px');
-      })
-      .on('mouseout', function () {
-        d3.select(this).style('fill', (d, i) => i < observedValues.length ? '#7cd6fd' : '#fca130'); // Restore color on mouseout
-        tooltip.transition()
-          .duration(100)
-          .style('opacity', 0);
-      })
-      .transition()
-      .duration(4000)
-      .style('opacity', 1) // Fade in the dots'
-      .style('cursor', 'pointer')
-
-    const tooltip = d3.select('#myChart')
-      .append('div')
-      .attr('class', 'tooltip')
-      .style('opacity', 0);
-
-      svg.append('g')
-  .attr('transform', `translate(0, ${svgHeight - margin.bottom})`)
-  .call(d3.axisBottom(xScale)
-    .tickFormat((d, i) => chartLabels[i])
-  )
-  .selectAll('text')
-  .attr('transform', 'rotate(-45)')
-  .style('text-anchor', 'end');
-
-    svg.append('g')
-      .attr('transform', `translate(${margin.left}, 0)`)
-      .call(d3.axisLeft(yScale));
-
-      svg.append('text')
-      .attr('transform', 'rotate(-90)')
-      .attr('y', margin.left / 3)
-      .attr('x', -svgHeight / 2)
-      .attr('dy', '0.5em')
-      .style('text-anchor', 'middle')
-      .text('Cubic Feet per Second (CFS)');
-
-    svg.append('text')
-      .attr('x', svgWidth / 2)
-      .attr('y', svgHeight - (margin.bottom) / 3)
-      .style('text-anchor', 'middle')
-      .text('Time');
-
-    // CSS Animation
-    // svg.style('opacity', 0)
-    //   .transition()
-    //   .duration(1500)
-    //   .style('opacity', 1);
     });
 </script>
 
 <div class="mx-auto p-8 text-center text-white">
     <h1 class="text-3xl">NW River Forecasting Dashboard</h1>
     <div class="grid sm:grid-cols-1 md:grid-cols-2 p-6 gap-3">
-        <div class="outline rounded-lg shadow-xl bg-white text-black">
-            <div class="text-black" id="myChart"></div>
+        <div class="outline rounded-3xl shadow-xl text-black pt-4 bg-slate-100">
+            {riverTitle}
+            <div class="p-4" id="myChart"></div>
         </div>
     </div>
 </div>
-
-<style>
-    .tooltip {
-        cursor: pointer;
-      position: absolute;
-      text-align: center;
-      width: 60px;
-      height: 28px;
-      padding: 2px;
-      font: 12px sans-serif;
-      background: lightsteelblue;
-      border: 0px;
-      border-radius: 8px;
-      pointer-events: none;
-    }
-
-
-  </style>
